4dc794446b7d9db41954f9829c3a2d5ff232498c
liamnoel007 <oasisamb@gmail.com>
2021-11-14 00:40:37 +0300

    1

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import TodoActions from '../TodoActions/TodoActions'
 import TodoHeader from '../TodoHeader/TodoHeader'
 import TodoRender from '../TodoRender/TodoRender'
+import './Todo.css'
 
 
 const Todo = () => {
