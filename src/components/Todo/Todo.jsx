0d7a88d5f3988e4227cfbe63693387990f78cf0f
liamnoel007 <oasisamb@gmail.com>
2021-11-14 21:42:37 +0300

    1

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import TodoActions from '../TodoActions/TodoActions'
 import TodoHeader from '../TodoHeader/TodoHeader'
 import TodoRender from '../TodoRender/TodoRender'
 import './Todo.css'
 
 
+const initialFormData = {
+    isEdit: false,
+    todoName: '',
+    todoNote: '',
+    isFinished: false,
+    id: '',
+    index: null
+}
+
+
 const Todo = () => {
+    const [tab, setTab] = useState(0)
+    const [isOpen, setIsOpen ] = useState(false)
+    const [isOpenDisplayTodo, setIsOpenDisplayTodo ] = useState(false)
+    const [todo, useTodos] = useState([])
+    const [formData, useFormData] = useState({
+        isEdit: false,
+        todoName: '',
+        todoNote: '',
+        isFinished: false,
+        id: '',
+        index: null
+    })
+
+    
+
     return(
         <div className='todo-wrapper'>
             <TodoHeader />
